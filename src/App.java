b70f2c252e2cd90182aed920b19b652f933eb7fb
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-09-29 20:13:40 +0200

    Taza echa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  █  .  .  .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .  .Aprende Java.  .  .  .  .  .  .  .  .  █  .  .  .  .  .  .  .  .  .  . .");
+        System.out.println(".  .  .Con Ejercicios.  .  .  .  .  .  .  .  █  .  .  .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .Santiago Luque Villanueva.  .  .███████████████████████████  .  .  .  .   . ");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  █████████████████  .  . ███ .  .  .  .  . .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  █████████████████  .  . ███ .  .  .  .  . .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  ███████████████████████████  .  .  .  .   .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  █████████████████ .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  █████████████████ .  .  .  .  .  .  .  .  .");
+        System.out.println(".  .  .  .  .  .  .  .  .  .  .  .  █████████████████ .  .  .  .  .  .  .  .  .");
+        System.out.println("                           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("                           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("                           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        
     }
 }
